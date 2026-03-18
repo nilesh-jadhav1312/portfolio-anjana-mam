238f2d9a3663d63cd19b3b47d36802e09bafe712
Nilesh Jadhav <nileshjadhav3301@gmail.com>
2026-03-18 19:10:45 +0530

    removed invalid wildcard route

--- backend/server.js
+++ backend/server.js
@@ -50,10 +50,6 @@ app.use("/api/contact-info", contactInfoRoutes);
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "public")));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running...");
